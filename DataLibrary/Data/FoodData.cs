b786edb2ed7ee4ac272794812d5d291f510d6e23
Yuriy <xaosmmm@gmail.com>
2021-01-03 19:31:11 +0200

    Cleared project from redundant data

---
+++ DataLibrary/Data/FoodData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLibrary.Db;
+using DataLibrary.Models;
+
+namespace DataLibrary.Data
+{
+    public class FoodData: IFoodData
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly ConnectionStringData _connectionString;
+
+        public FoodData(IDataAccess dataAccess, ConnectionStringData connectionString)
+        {
+            _dataAccess = dataAccess;
+            _connectionString = connectionString;
+        }
+
+        public Task<List<FoodModel>> GetFood()
+        {
+            return _dataAccess.LoadData<FoodModel, dynamic>("dbo.spFood_All", new { },
+                                     _connectionString.SqlConnectionName);
+
+        }
+
+        public FoodModel GetFoodById(int id)
+        {
+            return _dataAccess.LoadData<FoodModel, dynamic>("dbo.spFood_GetById", new {Id = id},
+                _connectionString.SqlConnectionName).Result.FirstOrDefault();
+
+        }
+    }
+}
---
+++ DataLibrary/Data/IFoodData.cs
@@ -0,0 +1,12 @@
+﻿using DataLibrary.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Data
+{
+    public interface IFoodData
+    {
+        Task<List<FoodModel>> GetFood();
+        public FoodModel GetFoodById(int id);
+    }
+}
